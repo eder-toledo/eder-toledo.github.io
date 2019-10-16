464274dd431f28aaeddbb50b62a7b7ca2058edc0
eder-toledo <me890506@gmail.com>
2019-10-15 21:40:59 -0500

    Se agregaron imagenes y se corrigio distribución

--- webPage/src/index.js
+++ webPage/src/index.js
@@ -1,14 +1,92 @@
 $(function () {
-    $('a[href*=\\#]').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            && location.hostname == this.hostname) {
-            var $target = $(this.hash);
-            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+
+    function desplazar(element) {
+
+        if (location.pathname.replace(/^\//, '') == element.pathname.replace(/^\//, '')
+            && location.hostname == element.hostname) {
+            var $target = $(element.hash);
+            $target = $target.length && $target || $('[name=' + element.hash.slice(1) + ']');
             if ($target.length) {
                 var targetOffset = $target.offset().top;
-                $('html,body').animate({ scrollTop: targetOffset }, 1000);
+                $('html,body').animate({ scrollTop: targetOffset });
                 return false;
             }
         }
+    }
+
+    $("#btnPage4").click(function () {
+        var element = this;
+        $("#page1").fadeOut();
+        $("#page2").fadeOut();
+        $("#page3").fadeOut();
+        $("#page4").fadeIn(2000);
+        setTimeout(function () {
+            desplazar(element);
+        }, 500);
     });
+
+    $(".btnPage1").click(function () {
+        var element = this;
+        $("#page2").fadeOut();
+        $("#page3").fadeOut();
+        $("#page4").fadeOut();
+        $("#page1").fadeIn(2000);
+        setTimeout(function () {
+            desplazar(element);
+        }, 500);
+    });
+
+    $(".btnPage2").click(function () {
+        var element = this;
+        $("#page1").fadeOut();
+        $("#page3").fadeOut();
+        $("#page4").fadeOut();
+        $("#page2").fadeIn(2000);
+        setTimeout(function () {
+            desplazar(element);
+        }, 500);
+    });
+
+    $(".btnPage3").click(function () {
+        var element = this;
+        $("#page1").fadeOut();
+        $("#page2").fadeOut();
+        $("#page4").fadeOut();
+        $("#page3").fadeIn(2000);
+        setTimeout(function () {
+            desplazar(element);
+        }, 500);
+    });
+
+    $('#btnPage4, .btnPage1, .btnPage2, .btnPage3').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+});
+
+new Chart(document.getElementById("myChart"), {
+    type: 'doughnut',
+    data: {
+        labels: ["", "", ""],
+        datasets: [
+            {
+                backgroundColor: ["#d63642", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                data: [60, 25, 15]
+            }
+        ]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            enabled: false
+        },
+        plugins: {
+            labels: {
+                render: 'percentage',
+                fontColor: ['white'],
+                precision: 2
+            }
+        }
+    }
 });
